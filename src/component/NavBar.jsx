fb4f7e99689c5984faa8124132301dae7b3f7fab
jaydenlin <jayden.weiyu@gmail.com>
2016-03-31 17:55:25 +0800

    add navbar

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
-
+import { Link } from 'react-router';
  var NavBar = React.createClass({
 	displayName: "NavBar",
+  contextTypes: {
+    router: React.PropTypes.object
+  },
  	render() {
+    var isRouteActive = this.context.router.isActive;
+   
  		return (
  			   <nav className="navbar navbar-default">
           <div className="container-fluid">
@@ -19,12 +24,11 @@ import React from 'react';
 
             <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
               <ul className="nav navbar-nav">
-                <li className="active"><a href="#">Link</a></li>
-
-                <li><a href="#">Link</a></li>
-                <li><a href="#">Link</a></li>
-                <li><a href="#">Link</a></li>
-          
+                
+                <li className={isRouteActive("/result") ? "active" : ""}><Link to={"/result"} >Result</Link></li>
+                <li className={isRouteActive("/info") ? "active" : ""}><Link to={"/info"} >Info</Link></li>
+                <li className={isRouteActive("/setting") ? "active" : ""}><Link to={"/setting"} >Setting</Link></li>
+               
               </ul>
              
               <ul className="nav navbar-nav navbar-right">
